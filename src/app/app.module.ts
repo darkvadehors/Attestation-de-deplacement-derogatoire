d7ac2ce548e3cdd25c756aee534a5bcc50c209de
JohnBen <darkvadehors@users.noreply.github.com>
2020-12-10 23:14:55 +0100

    ajoute du model UseSetting

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+
+// ReactiveFormss
+import { ReactiveFormsModule } from '@angular/forms';
+
+//QRCode
+import { QRCodeModule } from 'angularx-qrcode';
+
+// Ionic
+import { IonicModule } from '@ionic/angular';
+
+// Aes256
+
+//Firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+
+//Pipe
+import { ActivityPipe } from './shared/pipe/activity/activity.pipe';
+import { BacktimePipe } from './shared/pipe/time/backtime.pipe';
+
+//Component
+import { AppComponent } from './app.component';
+import { SetupComponent } from './features/setup/setup.component';
+import { WelcomeComponent } from './features/welcome/welcome.component';
+import { AttestationComponent } from './features/attestation/attestation.component';
+import { StorageService } from './service/storage/storage.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AttestationComponent,
+    SetupComponent,
+    WelcomeComponent,
+    ActivityPipe,
+    BacktimePipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    QRCodeModule,
+    IonicModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+  ],
+  providers: [StorageService, BacktimePipe, ActivityPipe],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/model/usersettings.ts
@@ -0,0 +1,11 @@
+export interface Usersettings {
+  firstname: string;
+  lastname: string;
+  dateofbirth: Date;
+  cityofbird: string;
+  adress: string;
+  city: string;
+  zipcode: number;
+  backtime: number;
+  lastchoice?: number;
+}
