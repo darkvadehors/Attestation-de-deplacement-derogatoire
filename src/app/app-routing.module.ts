167cd90041775050d299743f62356b4aed914c59
JohnBen <darkvadehors@users.noreply.github.com>
2020-12-15 19:51:32 +0100

    SLide => OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./features/tabs/tabs.modules').then(m => m.TabsModule)
   },
+  {
+    path: 'intro',
+    loadChildren: () => import('./features/slides/slides.modules').then(m => m.SlidesModule)
+  },
   // { path: '**', component: WelcomeComponent }, // Wildcard route for a 404 page
   { path: '**', redirectTo: '', pathMatch: 'full' }, // Wildcard route for a 404 page
 ];
--- src/app/features/attestation/attestation.modules.ts
+++ src/app/features/attestation/attestation.modules.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { AttestationComponent } from './attestation/attestation.component';
+import { AttestationComponent } from './containers/attestation/attestation.component';
 import { AttestationRoutingModule } from './attestation-routing.modules';
 //QRCode
 import { QRCodeModule } from 'angularx-qrcode';
