36e48402cde1c4df44e3ffc8ea644aa539b5bfac
JohnBen <darkvadehors@users.noreply.github.com>
2020-12-17 23:56:57 +0100

    carte avec nom de l'utilisateur

--- src/app/features/map/containers/map/map.component.ts
+++ src/app/features/map/containers/map/map.component.ts
@@ -1,31 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-
+import { Plugins } from '@capacitor/core';
+import { VariableService } from '../../../../service/variable/variable.service';
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
+
+
 export class MapComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  coords: any;
+
+  constructor(private http: HttpClient, private _varGlobal: VariableService) { }
 
   // Fonction d'initialisation du composant.
-  ngOnInit() {
+  async ngOnInit() {
+    this._varGlobal.loadVar();
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.coords = coordinates.coords;
     // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-    const myfrugalmap = L.map('frugalmap').setView([ 50.6311634, 3.0599573 ], 12);
+    const myfrugalmap = L.map('frugalmap').setView([ this.coords.latitude, this.coords.longitude ], 18);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'Frugal Map'
+      attribution: 'Attestation Covid 19'
     }).addTo(myfrugalmap);
 
-
+    const identifiant = this._varGlobal.setting.firstname + ' - ' + this._varGlobal.setting.lastname;
     const myIcon = L.icon({
       iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
     });
-    L.marker([ 50.6311634, 3.0599573 ], { icon: myIcon }).bindPopup('Je suis un Frugal Marqueur').addTo(myfrugalmap).openPopup();
+    L.marker([ this.coords.latitude, this.coords.longitude ], { icon: myIcon }).bindPopup(identifiant).addTo(myfrugalmap).openPopup();
 
   }
+
 }
