49829e383febfe2a628cb043013de641a24c4457
JohnBen <darkvadehors@users.noreply.github.com>
2020-12-17 23:31:38 +0100

    carte LEAFLET => OK

--- src/app/features/map/containers/map/map.component.ts
+++ src/app/features/map/containers/map/map.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-map',
@@ -7,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    // console.log('map is work');
+  // Fonction d'initialisation du composant.
+  ngOnInit() {
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myfrugalmap = L.map('frugalmap').setView([ 50.6311634, 3.0599573 ], 12);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Frugal Map'
+    }).addTo(myfrugalmap);
+
+
+    const myIcon = L.icon({
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    });
+    L.marker([ 50.6311634, 3.0599573 ], { icon: myIcon }).bindPopup('Je suis un Frugal Marqueur').addTo(myfrugalmap).openPopup();
+
   }
-
 }
--- src/app/features/map/map.modules.ts
+++ src/app/features/map/map.modules.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { MapRoutingModule } from './map-routing.modules';
 import { MapComponent } from './containers/map/map.component';
+
+
 
 @NgModule({
     declarations: [ MapComponent ],
     imports: [
         CommonModule,
+        HttpClientModule,
         MapRoutingModule,
         IonicModule,
-    ]
+    ],
+
 })
 export class MapModule { }
