21efdf63bd84c83e70fab5f3388edd41d2510352
JohnBen <darkvadehors@users.noreply.github.com>
2021-02-08 19:58:19 +0100

    supp environment inutile

--- src/app/service/pdfmake/pdfmake.service.ts
+++ src/app/service/pdfmake/pdfmake.service.ts
@@ -1,4 +1,3 @@
-import { environment } from './../../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { VariableService } from '../variable/variable.service';
