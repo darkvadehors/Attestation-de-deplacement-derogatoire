71b628bc078af9497e42a15d33a35d29d8836a93
JohnBen <darkvadehors@users.noreply.github.com>
2021-02-10 17:28:34 +0100

    creation class difinition

---
+++ src/app/service/pdfmake/definifion/couvre-feux.definition.spec.ts
@@ -0,0 +1,7 @@
+import { CouvreFeux.Definition } from './couvre-feux.definition';
+
+describe('CouvreFeux.Definition', () => {
+  it('should create an instance', () => {
+    expect(new CouvreFeux.Definition()).toBeTruthy();
+  });
+});
---
+++ src/app/service/pdfmake/definifion/couvre-feux.definition.ts
@@ -0,0 +1,2 @@
+export class CouvreFeux.Definition {
+}
