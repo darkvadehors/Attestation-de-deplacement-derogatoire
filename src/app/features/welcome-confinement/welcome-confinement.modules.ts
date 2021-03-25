a6c17c8ccec0a82fdeec8dcff859392e3b254873
darkvadehors <johnben.dev@gmail.com>
2021-03-25 10:30:21 +0100

    ajout component confinement

---
+++ src/app/features/welcome-confinement/welcome-confinement.modules.ts
@@ -0,0 +1,22 @@
+import { AttestationComponent } from '../attestation/containers/attestation/attestation.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+import { WelcomeConfinementComponent } from './containers/welcome-confinement/welcome-confinement.component';
+import { WelcomeConfinementRoutingModule } from './welcome-confinement-routing.modules';
+import { AttestationModule } from '../attestation/attestation.modules';
+
+
+@NgModule({
+    declarations: [ WelcomeConfinementComponent ],
+    imports: [
+        CommonModule,
+        WelcomeConfinementRoutingModule,
+        IonicModule,
+    ],
+    providers: [
+        AttestationComponent
+    ]
+})
+export class WelcomeConfinementModule { }
