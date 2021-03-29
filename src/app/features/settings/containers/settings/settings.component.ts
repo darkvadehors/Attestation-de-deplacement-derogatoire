8134bbdb3ed4204238e961c431f49e63cec89e7a
darkvadehors <johnben.dev@gmail.com>
2021-03-29 09:50:38 +0200

    refactoring et commantaires

--- src/app/features/settings/containers/settings/settings.component.ts
+++ src/app/features/settings/containers/settings/settings.component.ts
@@ -1,16 +1,18 @@
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
 import { AlertController, IonRadioGroup, IonRouterOutlet, ModalController } from '@ionic/angular';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { DesktopComponent } from 'src/app/features/desktop/containers/desktop.component';
+import { RouteService } from './../../../../service/route/route.service';
 import { LoadingService } from '../../../../service/loading/loading.service';
 import { UpdateIosService } from './../../../../service/updateIos/update-ios.service';
-import { Usersettings } from '../../../../model/usersettings';
 import { StorageService } from '../../../../service/storage/storage.service';
 import { VariableService } from '../../../../service/variable/variable.service';
+import { Usersettings } from '../../../../model/usersettings';
 import { version } from '../../../../../../package.json'
-import { DesktopComponent } from 'src/app/features/desktop/containers/desktop.component';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -21,15 +23,30 @@ export class SettingsComponent implements OnInit {
   @ViewChild('disabled') tabs: ElementRef;
   @ViewChild('screenModeRadio') radioGroup: IonRadioGroup
 
+  //  Observable status
+  routeStatus$: Observable<any> = this._routeStatus.getRouteStatus();
+
+  // initialize les variable de la route status
+  tabsName: string;
+
+
   //Get value on ionChange on IonRadioGroup
   selectedRadioGroup: any;
+
   //Get value on ionSelect on IonRadio item
   selectedRadioItem: any;
 
+  // initialize validation _form
   validations_form: FormGroup;
+
+  // initialize setting avec valeur par default undefined
   setting: Usersettings = undefined;
   save: boolean = false;
-  screenmode: number;
+
+  //  initialize affichage en mode PDF ou Tous anti PV avec valeur par default 1 -> TAP
+  screenmode?: number = 1;
+
+  // initialize vertion local
   versionLocal: string = version;
 
   constructor(
@@ -42,24 +59,24 @@ export class SettingsComponent implements OnInit {
     private _storage: StorageService,
     private _Update: SwUpdate,
     private _upDateIos: UpdateIosService,
-    private routerOutlet: IonRouterOutlet
+    private routerOutlet: IonRouterOutlet,
+    private _routeStatus: RouteService,
+
+    // ngZone pour les liens externe
+    // private _ngZone: NgZone,
   ) { }
 
   ngOnInit(): void {
+
+    // charge les variables Globales
     this._varGlobal.loadVar();
 
-    // console.log("var", this._varGlobal.setting);
+    // chargement de la route dans la variable tabsName pour le bouton submit
+    this.routeStatus$.subscribe((data) => {
+      this.tabsName = data?.tabsName;
+    });
 
-    if (this.screenmode == undefined) {
-      // console.log("c'est null");
-      this.screenmode = 1;
-      // this._varGlobal.setting.screenmode = this.screenMode;
-      //
-      // console.log("this._varGlobal.setting.screenmode", this._varGlobal.setting.screenmode);
-      // this.screenMode = this._varGlobal.setting.screenmode;
-      // console.log("this.screen", this.screenmode);
-    }
-
+    // Validator pour le formulaire de settings
     this.validations_form = this.formBuilder.group({
       firstname: new FormControl(this._varGlobal.setting?.firstname, Validators.required),
       lastname: new FormControl(this._varGlobal.setting?.lastname, Validators.required),
@@ -83,16 +100,21 @@ export class SettingsComponent implements OnInit {
     });
   }
 
+  // avant d'entre dans la page
   ionViewDidEnter() {
+    // fermeture du loader
+    this.loading.hide();
+    // chargement des variables
     this._varGlobal.loadVar();
 
-    this.loading.hide();
-    this._Update.available.subscribe((event) => {
-      // console.log('object update');
-      // confirm('Nouvelle version');
-    })
+    // FIXME controler l'utilité de l'update
+    // this._Update.available.subscribe((event) => {
+    //   console.log('object update');
+    //   // confirm('Nouvelle version');
+    // })
   }
 
+  // messages de validation
   validation_messages = {
     'firstname': [
       { type: 'required', message: 'Le prénom est obligatoire.' }
@@ -122,46 +144,77 @@ export class SettingsComponent implements OnInit {
   };
 
   onSubmit(): any {
-    // this.loading.show();
-
+    // envoie des valeurs du fomrulaire dans la VariableService
     this._varGlobal.setting = this.validations_form.value;
 
+    // ajoute d'une key ac (ac -> Attestation Covid) dans le localstorage contenant tous les réglages
     this._storage.saveLocal('ac', this.validations_form.value);
+    // ajout de la key version au cas ou le updater serait desactive par un OS
     this._upDateIos.createVersionIos();
-
+    // ajoute de la key setok pour valider la première configuration
     this._storage.saveLocal('setok', '1');
 
-    this._router.navigate([ '' ]);
+    // redirige vers le component en fonction de la tabsName charge par l'observable
+    this._router.navigate([ this.tabsName ]);
   }
 
   ionViewWillLeave() {
+    // avant de sortir de l'onglet affichage du loader
     this.loading.show();
   }
 
-  updateIos() {
-    this.loading.show();
-    location.reload();
+  async updateIos() {
+
+    const alert = await this.alertCtl.create({
+
+      header: 'Attention!',
+      message: 'Vérifier que vous êtes bien <strong>connecté</strong> à internet!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            return;
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            // affichage du loader
+            this.loading.show();
+            // rechargement de la page pour force la mise a jour
+            location.reload();
+          }
+        }
+      ]
+    });
+    // affichage de l'alerte
+    await alert.present();
+
   }
 
-  radioGroupChange(event) {
-    // console.log("radioGroupChange", event.detail);
+
+  radioGroupChange(event: any) {
+    console.log("radioGroupChange", event.detail);
     this.selectedRadioGroup = event.detail;
   }
 
   radioFocus() {
-    // console.log("radioFocus");
+    console.log("radioFocus");
   }
-  radioSelect(event) {
-    // console.log("radioSelect", event.detail);
+  radioSelect(event: any) {
+    console.log("radioSelect", event.detail);
     this.selectedRadioItem = event.detail;
   }
   radioBlur() {
-    // console.log("radioBlur");
+    console.log("radioBlur");
   }
 
   revoirIntro() {
+    // suppression de la key intro du localstorage
     this._storage.deleteLocal("intro");
-    location.reload();
+    // redirection vers le component intro
+    this._router.navigate([ 'intro' ])
 
   }
 
@@ -170,6 +223,7 @@ export class SettingsComponent implements OnInit {
       component: DesktopComponent,
       cssClass: 'my-custom-class',
       presentingElement: this.routerOutlet.nativeEl,
+      // envoie d'une variable avec componentProps pour afficher le bouton "fermer" dans la page
       componentProps: {
         'button': 1,
       }
